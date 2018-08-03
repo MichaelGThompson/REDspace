e9e7245771da65643e3648d84a4fb7164964c699
Michael Thompson <mgt1964@gmail.com>
2018-08-02 23:31:19 -0300

    Added label/title on player view

--- REDspace Challenge/ViewControllers/HorizontalScrollViewController.swift
+++ REDspace Challenge/ViewControllers/HorizontalScrollViewController.swift
@@ -46,7 +46,6 @@ class HorizontalScrollViewController: UIViewController {
                 if let playerLayer = firstPlayer.playerLayer {
                     self.view.layer.addSublayer(playerLayer)
                     playerLayer.frame = self.view.frame
-                    //self.navigationItem.title = "Video Feed 1"
                     firstPlayer.playerView.titleLabel.text = "Video Feed 1"
                     firstPlayer.play()
                 }
@@ -90,8 +89,6 @@ extension HorizontalScrollViewController: UIScrollViewDelegate {
             if let currentPlayerLayer = players[currentPlayerIndex].playerLayer, let pageIndexPlayerLayer = players[pageIndex].playerLayer {
                 view.layer.replaceSublayer(currentPlayerLayer, with: pageIndexPlayerLayer)
                 pageIndexPlayerLayer.frame = view.frame
-//                players[pageIndex].play()
-//                navigationItem.title = "Video Feed \(pageIndex + 1)"
                 players[pageIndex].playerView.titleLabel.text = "Video Feed \(pageIndex + 1)"
                 players[pageIndex].playFromBeginning()
             }
